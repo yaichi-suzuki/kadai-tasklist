fdc6c47fb89925b8aa0732950e7f4e46bffd8dcf
yaichi.suzuki <proappler@gmail.com>
2020-05-05 12:27:24 +0000

    set_tasks削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:edit, :show, :update, :destroy]
   
@@ -47,10 +46,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
